2817366b6b03c9f66d7acae179e27f42bb7a2a54
Matt Gabiou <mattgabiou@yahoo.com>
2015-12-14 00:17:43 -0600

    Changes
    Talkin to the Devil

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main()
     cout << "1. Get the devil!" << endl;
     cout << "2. Where is the devil?" << endl;
     cout << "3. Found the devil!" << endl;
+    cout << "4. Talkin to the devil!" << endl;
 
     return 0;
 }
